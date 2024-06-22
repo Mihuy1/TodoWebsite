44c6831463c3a21620a8a5f9b328ad9934c6032b
Mihuy1 <patrik.hyytiainen@protonmail.com>
2024-06-22 12:41:35 +0300

    Create AddTaskForm for login to creating a new task

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,46 +1,72 @@
 import Task from './Task.jsx';
+import AddTaskForm from './AddTaskForm.jsx';
 
-const todoArray = [
+import React, {useState} from 'react';
+
+const initialTodoArray = [
   {
     id: 1,
     name: 'Task 1',
     details: 'This is Task 1',
+    status: true,
   },
   {
     id: 2,
     name: 'Task 2',
     details: 'This is Task 2',
+    status: false,
   },
   {
     id: 3,
     name: 'Task 3',
     details: 'This is Task 3',
+    status: false,
   },
   {
     id: 4,
     name: 'Task 4',
     details: 'This is task 4',
+    status: false,
   },
 ];
 
 const Home = () => {
+  const [todoArray, setTodoArray] = useState(initialTodoArray);
+  const [newTaskName, setNewTaskName] = useState('');
+
+  const handleStatusChange = (id, newStatus) => {
+    const updatedTasks = todoArray.map((task) =>
+      task.id === id ? {...task, status: newStatus} : task,
+    );
+    setTodoArray(updatedTasks);
+  };
+
+  const handleInputChange = (e) => {
+    setNewTaskName(e.target.value);
+  };
+
+  const handleAddTask = (event) => {
+    event.preventDefault();
+    const newTask = {
+      id: todoArray.length + 1,
+      name: newTaskName,
+      details: `This is ${newTaskName}`,
+      status: false,
+    };
+    setTodoArray([...todoArray, newTask]);
+    setNewTaskName('');
+  };
+
   return (
     <>
       <h2>Tasks</h2>
-      <form>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <AddTaskForm
+        newTaskName={newTaskName}
+        handleInputChange={handleInputChange}
+        handleAddTask={handleAddTask}
+      />
       {todoArray.map((item) => (
-        <Task key={item.id} item={item} />
+        <Task key={item.id} item={item} onStatusChange={handleStatusChange} />
       ))}
     </>
   );
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,13 +1,31 @@
 import PropTypes from 'prop-types';
 
 const Task = (props) => {
-  const {item} = props;
+  const {item, onStatusChange} = props;
 
-  return <h3>{item.name}</h3>;
+  const handleCheckboxChange = (event) => {
+    onStatusChange(item.id, event.target.checked);
+  };
+
+  return (
+    <>
+      <h4>{item.name}</h4>
+      <input
+        type="checkbox"
+        checked={item.status}
+        onChange={handleCheckboxChange}
+      />{' '}
+      {item.status ? 'True' : 'False'}
+    </>
+  );
 };
 
 Task.propTypes = {
-  item: PropTypes.object.isRequired,
+  item: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    status: PropTypes.bool.isRequired,
+  }).isRequired,
+  onStatusChange: PropTypes.func.isRequired,
 };
 
 export default Task;
